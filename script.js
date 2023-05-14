0f35edfac15d76f0b4301a2c879eb5a75c1c6ae1
Marc Norris <shaky411@googlemail.com>
2023-05-14 20:48:43 +0100

    added clear button

--- script.js
+++ script.js
@@ -1,16 +1,27 @@
 const jokeBtn = document.getElementById('joke-btn');
 const joke = document.getElementById('joke-container');
+const clear = document.getElementById('clear');
 
 
 async function logJSONData() {
     const response = await fetch("https://api.chucknorris.io/jokes/random");
     const jsonData = await response.json();
-    // console.log(jsonData.value);
+    // console.log(jsonData);
 
     joke.classList.remove('hidden');
+    clear.classList.remove('hidden');
     joke.innerHTML = jsonData.value
     
   };
 
+  function clearData() {
+    joke.innerHTML = ''
+    joke.classList.add('hidden');
+    clear.classList.add('hidden');
+  }
+
 
   jokeBtn.addEventListener('click', logJSONData);
+  clear.addEventListener('click', clearData);
+
+  
