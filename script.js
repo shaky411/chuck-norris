9760b090f287a368f525200d078c337ce2f05ef0
Marc Norris <shaky411@googlemail.com>
2023-05-16 15:02:16 +0100

    updated delete function

--- script.js
+++ script.js
@@ -1,127 +1,122 @@
-const jokeBtn = document.getElementById('joke-btn');
-const joke = document.getElementById('joke-container');
-const clear = document.getElementById('clear');
-const like = document.getElementById('like');
-const copy = document.getElementById('copy');
-const favBtn = document.getElementById('favorites');
-const favorites = document.getElementById('favorites-container');
-const close = document.getElementById('close');
-const deleteFav = document.getElementById('delete-all');
-const favList = document.getElementById('fav-list');
+const jokeBtn = document.getElementById("joke-btn");
+const joke = document.getElementById("joke-container");
+const clear = document.getElementById("clear");
+const like = document.getElementById("like");
+const copy = document.getElementById("copy");
+const favBtn = document.getElementById("favorites");
+const favorites = document.getElementById("favorites-container");
+const close = document.getElementById("close");
+const deleteFav = document.getElementById("delete-all");
+const favList = document.getElementById("fav-list");
+const items = document.getElementById("item-list");
 
-
-const localStorageJokes = JSON.parse(localStorage.getItem('jokes'));
+const localStorageJokes = JSON.parse(localStorage.getItem("jokes"));
 
 // When the like button is clicked run this code
 function likeJoke(event) {
-    event.preventDefault();
+  event.preventDefault();
 
-    const marc = {
-        id: generateID(),
-        text: joke.innerHTML
-    }
+  const marc = {
+    id: generateID(),
+    text: joke.innerHTML,
+  };
 
-    jokes.push(marc);
-    addJokeDOM(marc);
-    updateLocalStorage();
-    
+  jokes.push(marc);
+  addJokeDOM(marc);
+  updateLocalStorage();
 }
 
 function copyJoke() {
-    navigator.clipboard.writeText(joke.innerHTML);
-    alert('Joke copied to clipboard');
+  navigator.clipboard.writeText(joke.innerHTML);
+  alert("Joke copied to clipboard");
 }
 
 // Generate random ID
-function generateID () {
-    return Math.floor(Math.random() * 100000000);
+function generateID() {
+  return Math.floor(Math.random() * 100000000);
 }
 
-let jokes = 
-localStorage.getItem('jokes') !== null ? 
-localStorageJokes : [];
-console.log(jokes)
+let jokes = localStorage.getItem("jokes") !== null ? localStorageJokes : [];
+console.log(jokes);
 
 function addJokeDOM(marc) {
-    const favEl = document.createElement('li');
-    favEl.innerHTML = `${marc.text} <button class="delete-btn" onClick="deleteFavJoke(${marc.id})"><i class="fa-solid fa-trash-can"></i></button>`
+  const favEl = document.createElement("li");
+  favEl.innerHTML = `${marc.text} <button class="delete-btn" onClick="deleteFavJoke(${marc.id})"><i class="fa-solid fa-trash-can"></i></button>`;
 
-    favList.appendChild(favEl);
+  favList.appendChild(favEl);
 }
 
 function deleteFavJoke(id) {
-    jokes = jokes.filter(marc => marc.id
-        !== id);
 
-        console.log(jokes)
+  jokes = jokes.filter((marc) => marc.id !== id);
+  console.log(jokes);
 
-const ul = document.querySelector('ul');
-const li = document.querySelector(`li`);
-
-ul.removeChild(li);
-
-        
-        updateLocalStorage();
-       
+  const favEl = document.getElementById(id);
+  if (favEl) {
+    favEl.remove()
   }
 
+//   const ul = document.querySelector("ul");
+//   const li = document.querySelector(`li`);
+
+//   ul.removeChild(li);
+
+  updateLocalStorage();
+}
 
 // Fetches joke when new joke button is clicked
 async function logJSONData() {
-    const response = await fetch("https://api.chucknorris.io/jokes/random");
-    const jsonData = await response.json();
-    // console.log(jsonData);
+  const response = await fetch("https://api.chucknorris.io/jokes/random");
+  const jsonData = await response.json();
+  // console.log(jsonData);
 
-    joke.classList.remove('hidden');
-    clear.classList.remove('hidden');
-    like.classList.remove('hidden');
-    copy.classList.remove('hidden');
-    favorites.classList.add('hidden');
+  joke.classList.remove("hidden");
+  clear.classList.remove("hidden");
+  like.classList.remove("hidden");
+  copy.classList.remove("hidden");
+  favorites.classList.add("hidden");
 
-    joke.innerHTML = jsonData.value
-    
-  };
+  joke.innerHTML = jsonData.value;
+}
 
 //   clears the currently displayed joke
-  function clearData() {
-    joke.innerHTML = ''
-    joke.classList.add('hidden');
-    clear.classList.add('hidden');
-    like.classList.add('hidden');
-    copy.classList.add('hidden');
-  }
+function clearData() {
+  joke.innerHTML = "";
+  joke.classList.add("hidden");
+  clear.classList.add("hidden");
+  like.classList.add("hidden");
+  copy.classList.add("hidden");
+}
 
-  function getFavJoke() {
-    
-  }
+function getFavJoke() {}
 
 // show favourites section
-  function showFav() {
-    favorites.classList.remove('hidden');
-    joke.classList.add('hidden');
-    clear.classList.add('hidden');
-    like.classList.add('hidden');
-    copy.classList.add('hidden');
-  }
+function showFav() {
+  favorites.classList.remove("hidden");
+  joke.classList.add("hidden");
+  clear.classList.add("hidden");
+  like.classList.add("hidden");
+  copy.classList.add("hidden");
+}
 
 //  hide favourites section
-  function hideFav() {
-    favorites.classList.add('hidden');
-  }
-
-  function updateLocalStorage() {
-    localStorage.setItem('jokes', JSON.stringify(jokes));
+function hideFav() {
+  favorites.classList.add("hidden");
 }
-  
+
+function updateLocalStorage() {
+  localStorage.setItem("jokes", JSON.stringify(jokes));
+}
+
 function init() {
-    jokes.forEach(addJokeDOM);
+  jokes.forEach(addJokeDOM);
 }
 
-init()
+init();
 
-  jokeBtn.addEventListener('click', logJSONData);
-  clear.addEventListener('click', clearData);
-  favBtn.addEventListener('click', showFav);
-  close.addEventListener('click', hideFav);
-  like.addEventListener('click', likeJoke);
-  copy.addEventListener('click', copyJoke);
+jokeBtn.addEventListener("click", logJSONData);
+clear.addEventListener("click", clearData);
+favBtn.addEventListener("click", showFav);
+close.addEventListener("click", hideFav);
+like.addEventListener("click", likeJoke);
+copy.addEventListener("click", copyJoke);
