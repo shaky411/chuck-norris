14aaba796b4281d54480764b86ea6b6b1bb8af13
Marc Norris <shaky411@googlemail.com>
2023-05-17 13:30:14 +0100

    minor update

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,21 @@ module.exports = {
   content: ['./*.html'],
   theme: {
     extend: {
+      keyframes: {
+        "fade-in-down": {
+          "0%": {
+            opacity: "0",
+            transform: "translateY(-75px)",
+          },
+          "100%": {
+            opacity: "1",
+            transform: "translateY(0)",
+          },
+        },
+      },
+      animation: {
+        'fade-in-down': 'fade-in-down 2.0s ease-out'
+      },
       fontFamily: {
         nunito: ["Nunito", "sans-serif"],
         oxygen: ["Oxygen", "sans-serif"],
@@ -17,5 +32,6 @@ module.exports = {
       },
     },
   },
+  variants: {},
   plugins: [],
 }
